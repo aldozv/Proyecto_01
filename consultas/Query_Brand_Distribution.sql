c6a16ed7794c0510f2a85751d139e0a4ba10cae0
Aldo <aldoz80@hotmail.com>
2026-08-24 15:20:18 -0500

    Agregar 2 consultas nuevas

--- consultas/Query_Brand_Distribution.sql
+++ consultas/Query_Brand_Distribution.sql
@@ -15,7 +15,7 @@ select
     sum(hl) as sum_hl
 from brewdat_uc_mazana_dev.slv_maz_salesdata_salesdatadata_adb.pe_portfolio_hm_brand_distribution v 
 left join brewdat_uc_maz_scus_weu_sales_dev_ds.gld_maz_sales_portfolio_pe.pe_portfolio_material m on v.material_id = m.material_id 
--- where v.indicadores_comerciales = 1
+where v.indicadores_comerciales = 1
 -- where v.mes in ('202601','202602','202603','202604','202605','202606','202607','202608')
 -- and day(v.fecha_venta) <= 15
 where v.fecha_venta between '2025-01-01' and '2026-07-31'
